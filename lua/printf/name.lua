749b4ce414e727ce3d591c96990bc4f116d5121a
Declan Mullen <declanmullen3.14@gmail.com>
2024-04-10 10:05:28 -0700

    Added comments to the variable name finding function

--- lua/printf/name.lua
+++ lua/printf/name.lua
@@ -5,13 +5,15 @@ local utils = require('printf.utils')
 --- Get the qualified name of the variable under the cursor.
 --- @return string
 M.get_var_qualified_name = function()
-	local node = vim.treesitter.get_node()
 	local types = { 'identifier', 'field_identifier', 'field_expression', 'property' }
 
+	-- Check if the cursor is starting on a variable.
+	local node = vim.treesitter.get_node()
 	if not node or not utils.contains(types, node:type()) then
 		return ''
 	end
 
+	-- Traverse the parent nodes until the parent is no longer part of the variable.
 	local parent = node:parent()
 	while parent do
 		if not utils.contains(types, parent:type()) then
