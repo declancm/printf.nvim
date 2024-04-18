f4ee9177816a0ac2418c8848c40b66bbff1f811c
Declan Mullen <declanmullen3.14@gmail.com>
2024-04-17 22:31:48 -0700

    Implemented automatic pointer dereferencing

--- lua/printf/format.lua
+++ lua/printf/format.lua
@@ -9,7 +9,13 @@ local config = require('printf.config')
 M.get_format_specifier = function(type)
 	-- Initialize the return values
 	local format = nil
-	local operators = {}
+	local operators = {
+		left = '',
+		right = ''
+	}
+
+	-- Counter to keep track of how many dereferences occur to prevent infinite loops.
+	local dereference_count = 0
 
 	-- Remove keywords that don't have an effect on the type format specifier.
 	type = type:gsub('volatile ', '')
@@ -17,39 +23,41 @@ M.get_format_specifier = function(type)
 	type = type:gsub('static ', '')
 	type = type:gsub('const ', '')
 
-	-- Return the format specifier.
-	if type == '_Bool' or type == 'bool' then
-		format = 'd'
-	elseif type:match('^char%[%d+%]$') then
-		format = 's'
-	elseif type == 'char *' and config.options.print_var.char_ptr_strings then
-		format = 's'
-	elseif type == 'size_t' then
-		format = 'zu'
-	elseif type == 'ssize_t' then
-		format = 'zd'
-	elseif type:match('^u?int%d+_t$') then
-		local size = type:match('^u?int(%d+)_t$')
-		format = '" PRI' .. type:sub(1, 1) .. size .. ' "'
-	elseif type == 'inptr_t' then
-		format = '" PRIdPTR "'
-	elseif type == 'uinptr_t' then
-		format = '" PRIuPTR "'
-	elseif type:match(' %*$') then
-		format = 'p'
-		operators.left = '(void *)'
-	elseif type:match('%[%d+%]$') then
-		format = 'p'
-		operators.left = '(void *)'
-	else
-		-- Check if signed or unsigned.
-		local count
-		type, count = type:gsub('unsigned ', '', 1)
-		local unsigned = count > 0
-		type, count = type:gsub('signed ', '', 1)
-		local signed = count > 0
+	-- Check if signed or unsigned.
+	local count
+	type, count = type:gsub('unsigned ', '')
+	local unsigned = count > 0
+	type, count = type:gsub('signed ', '')
+	local signed = count > 0
 
-		if type == 'char' then
+	while not format do
+		-- Strip outer whitespace.
+		type = type:match('^%s*(.-)%s*$')
+
+		-- Look for a type match and get the format.
+		if type:match('^char%[%d+%]$') then
+			format = 's'
+		elseif type == 'char *' and config.options.print_var.char_ptr_strings then
+			format = 's'
+		elseif type:match('%[%d+%]$') then
+			format = 'p'
+			operators.left = '(void *)'
+		elseif type == '_Bool' or type == 'bool' then
+			format = 'd'
+		elseif type == 'size_t' then
+			format = 'zu'
+		elseif type == 'ssize_t' then
+			format = 'zd'
+		elseif type:match('^u?int%d+_t$') then
+			format = '" PRI' .. type:sub(1, 1) .. type:match('^u?int(%d+)_t$') .. ' "'
+		elseif type == 'inptr_t' then
+			format = '" PRIdPTR "'
+		elseif type == 'uinptr_t' then
+			format = '" PRIuPTR "'
+		elseif type:match(' %*$') and not config.options.print_var.dereference_pointers then
+			format = 'p'
+			operators.left = '(void *)' .. operators.left
+		elseif type == 'char' then
 			if unsigned then
 				format = 'hhu'
 			elseif signed then
@@ -71,6 +79,17 @@ M.get_format_specifier = function(type)
 			format = 'f'
 		elseif type == 'long double' then
 			format = 'Lf'
+		else
+			-- The type didn't match a supported type. If the variable is a pointer,
+			-- dereference it and check for a match. Maximum supported depth is 256 (to
+			-- prevent infinite loops) which is the minimum recommended for a C++ compiler.
+			if config.options.print_var.dereference_pointers and type:match('%*$') and dereference_count < 256 then
+				type = type:match('(.*)%*$')
+				operators.left = '*' .. operators.left
+				dereference_count = dereference_count + 1
+			else
+				break
+			end
 		end
 	end
 
