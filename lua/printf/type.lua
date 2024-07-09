c23163271b1ff30ae9dfe2255e67a28efc924a85
Declan Mullen <declanmullen3.14@gmail.com>
2024-07-09 14:49:30 -0700

    fix: handle multiple LSP clients

--- lua/printf/type.lua
+++ lua/printf/type.lua
@@ -1,39 +1,37 @@
 local M = {}
 
 --- Get the variable type information from the LSP hover method.
---- @param ls_name string
 --- @param hover_value string
 --- @return string
-local get_type_from_hover = function(ls_name, hover_value)
-	local type
-
-	if ls_name == 'clangd' then
-		type = hover_value:match('Type: `(.-)`') or ''
-		type = type:match(' %(aka (.*)%)') or type
-	end
-
-	return type
+local get_type_from_hover = function(hover_value)
+    local type = hover_value:match('Type: `(.-)`') or ''
+    return type:match(' %(aka (.*)%)') or type
 end
 
 --- Gets the type of the variable under the cursor.
 --- @return string|nil
 M.get_var_type = function()
-	-- Get the attached language server name.
-	local lsp_clients = vim.lsp.get_clients()
-	if not lsp_clients or not next(lsp_clients) then
-		return nil
-	end
-	local ls_name = lsp_clients[1].name
+    -- Get the client ID for clangd.
+    local clangd_id = nil
+    for _, client in pairs(vim.lsp.get_clients()) do
+        if client.name == 'clangd' then
+            clangd_id = client.id
+            break
+        end
+    end
+    if clangd_id == nil then
+        return nil
+    end
 
-	-- Get the LSP hover method since that has type data.
-	local params = vim.lsp.util.make_position_params(vim.api.nvim_get_current_win())
-	local response = vim.lsp.buf_request_sync(vim.api.nvim_get_current_buf(), 'textDocument/hover', params)
-	if not response or not next(response) then
-		return nil
-	end
-	local value = response[1].result.contents.value
+    -- Get the LSP hover method since that has type data.
+    local params = vim.lsp.util.make_position_params(vim.api.nvim_get_current_win())
+    local response = vim.lsp.buf_request_sync(vim.api.nvim_get_current_buf(), 'textDocument/hover', params)
+    if not response or not response[clangd_id] then
+        return nil
+    end
+    local value = response[clangd_id].result.contents.value
 
-	return get_type_from_hover(ls_name, value)
+    return get_type_from_hover(value)
 end
 
 return M
